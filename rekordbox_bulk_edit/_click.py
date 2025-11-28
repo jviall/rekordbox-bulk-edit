8b43b6fb5d01a05a947bd50e59a034a44f006e70
jviall <jamesviall@gmail.com>
2025-11-28 15:50:05 -0600

    feat: replace Read command with Search command using the CollectionQuery class

---
+++ rekordbox_bulk_edit/_click.py
@@ -0,0 +1,101 @@
+from enum import Enum
+
+import click
+
+
+class OutputChoice(Enum):
+    SILENT = 0
+    IDS = 1
+    INFO = 2
+
+
+output_option = click.option(
+    "--output",
+    default="info",
+    type=click.Choice(OutputChoice, case_sensitive=False),
+    help="Configures the kind of output you want from the command, if any. The 'ids' option can be used to pipe a list of resulting content IDs into to another command.",
+)
+
+track_ids_argument = click.argument(
+    "track-ids",
+    type=str,
+    required=False,
+)
+
+global_click_filters = [
+    click.option(
+        "--track-id",
+        type=str,
+        multiple=True,
+        help="Filter by the given Database Track ID",
+    ),
+    click.option(
+        "--title",
+        type=str,
+        multiple=True,
+        help="Find track names that include this value",
+    ),
+    click.option(
+        "--exact-title",
+        type=str,
+        multiple=True,
+        help="Find track names that are exactly this value",
+    ),
+    click.option(
+        "--playlist",
+        type=str,
+        multiple=True,
+        help="Find tracks in playlists whose names include this value",
+    ),
+    click.option(
+        "--exact-playlist",
+        type=str,
+        multiple=True,
+        help="Find tracks in the plalist whose name is exactly this value",
+    ),
+    click.option(
+        "--artist",
+        type=str,
+        multiple=True,
+        help="Find tracks whose Artist names include this value",
+    ),
+    click.option(
+        "--exact-artist",
+        type=str,
+        multiple=True,
+        help="Find tracks whose Artists names are exactly this value",
+    ),
+    click.option(
+        "--album",
+        type=str,
+        multiple=True,
+        help="Find tracks whose Album names include this value",
+    ),
+    click.option(
+        "--exact-album",
+        type=str,
+        multiple=True,
+        help="Find tracks whose Album names are exactly this value",
+    ),
+    click.option(
+        "--format",
+        type=click.Choice(["mp3", "flac", "aiff", "wav", "m4a"], case_sensitive=False),
+        multiple=True,
+        help="Find tracks of this format",
+    ),
+    click.option(
+        "--match-all",
+        type=bool,
+        is_flag=True,
+        help="Results must match all given filters",
+    ),
+]
+
+
+def add_click_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+
+    return _add_options
